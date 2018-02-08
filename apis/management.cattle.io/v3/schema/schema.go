a166ea594ea5834dd2300f459a00058171e6f4fa
gitlawr <lawrleegle@gmail.com>
2018-02-08 15:07:36 +0800

    update pipeline types

--- apis/management.cattle.io/v3/schema/schema.go
+++ apis/management.cattle.io/v3/schema/schema.go
@@ -235,7 +235,7 @@ func pipelineTypes(schema *types.Schemas) *types.Schemas {
 		AddMapperForType(&Version, &v3.SourceCodeCredential{},
 			m.DisplayName{}).
 		//&m.Move{From: "type", To: "kind"}).
-		AddMapperForType(&Version, &v3.SourceCodeRepository{}).
+		AddMapperForType(&Version, &v3.SourceCodeRepository{}, m.DisplayName{}).
 		//&m.Move{From: "type", To: "kind"}).
 		AddMapperForType(&Version, &v3.PipelineExecutionLog{}).
 		MustImport(&Version, v3.AuthAppInput{}).
