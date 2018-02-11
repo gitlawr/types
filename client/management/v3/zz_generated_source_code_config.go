856ee3e2fdbe8e313cf6018fe6a9485c0883ea54
gitlawr <lawrleegle@gmail.com>
2018-02-11 12:01:30 +0800

    update generated

--- client/management/v3/zz_generated_source_code_config.go
+++ client/management/v3/zz_generated_source_code_config.go
@@ -3,12 +3,14 @@ package client
 const (
 	SourceCodeConfigType                        = "sourceCodeConfig"
 	SourceCodeConfigFieldBranch                 = "branch"
+	SourceCodeConfigFieldBranchCondition        = "branchCondition"
 	SourceCodeConfigFieldSourceCodeCredentialId = "sourceCodeCredentialId"
 	SourceCodeConfigFieldUrl                    = "url"
 )
 
 type SourceCodeConfig struct {
 	Branch                 string `json:"branch,omitempty"`
+	BranchCondition        string `json:"branchCondition,omitempty"`
 	SourceCodeCredentialId string `json:"sourceCodeCredentialId,omitempty"`
 	Url                    string `json:"url,omitempty"`
 }
