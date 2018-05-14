4362e1ae40c0d72ba892e0c11003ebb79a547b23
gitlawr <lawrleegle@gmail.com>
2018-05-14 11:44:41 +0800

    update generated

--- client/management/v3/zz_generated_publish_image_config.go
+++ client/management/v3/zz_generated_publish_image_config.go
@@ -11,6 +11,6 @@ const (
 type PublishImageConfig struct {
 	BuildContext   string `json:"buildContext,omitempty" yaml:"buildContext,omitempty"`
 	DockerfilePath string `json:"dockerfilePath,omitempty" yaml:"dockerfilePath,omitempty"`
-	Push           string `json:"push,omitempty" yaml:"push,omitempty"`
+	Push           bool   `json:"push,omitempty" yaml:"push,omitempty"`
 	Tag            string `json:"tag,omitempty" yaml:"tag,omitempty"`
 }
