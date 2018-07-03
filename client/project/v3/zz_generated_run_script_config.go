0b81774114aa14eca4fbb23eabdbac3e952ace8e
gitlawr <lawrleegle@gmail.com>
2018-07-03 10:55:03 +0800

    update generated

--- client/project/v3/zz_generated_run_script_config.go
+++ client/project/v3/zz_generated_run_script_config.go
@@ -2,23 +2,11 @@ package client
 
 const (
 	RunScriptConfigType             = "runScriptConfig"
-	RunScriptConfigFieldCommand     = "command"
-	RunScriptConfigFieldEntrypoint  = "entrypoint"
-	RunScriptConfigFieldEnv         = "env"
-	RunScriptConfigFieldEnvFrom     = "envFrom"
 	RunScriptConfigFieldImage       = "image"
-	RunScriptConfigFieldIsShell     = "isShell"
-	RunScriptConfigFieldPrivileged  = "privileged"
 	RunScriptConfigFieldShellScript = "shellScript"
 )
 
 type RunScriptConfig struct {
-	Command     string            `json:"command,omitempty" yaml:"command,omitempty"`
-	Entrypoint  string            `json:"entrypoint,omitempty" yaml:"entrypoint,omitempty"`
-	Env         map[string]string `json:"env,omitempty" yaml:"env,omitempty"`
-	EnvFrom     []EnvFrom         `json:"envFrom,omitempty" yaml:"envFrom,omitempty"`
-	Image       string            `json:"image,omitempty" yaml:"image,omitempty"`
-	IsShell     bool              `json:"isShell,omitempty" yaml:"isShell,omitempty"`
-	Privileged  bool              `json:"privileged,omitempty" yaml:"privileged,omitempty"`
-	ShellScript string            `json:"shellScript,omitempty" yaml:"shellScript,omitempty"`
+	Image       string `json:"image,omitempty" yaml:"image,omitempty"`
+	ShellScript string `json:"shellScript,omitempty" yaml:"shellScript,omitempty"`
 }
--- client/project/v3/zz_generated_step.go
+++ client/project/v3/zz_generated_step.go
@@ -3,6 +3,9 @@ package client
 const (
 	StepType                    = "step"
 	StepFieldApplyYamlConfig    = "applyYamlConfig"
+	StepFieldEnv                = "env"
+	StepFieldEnvFrom            = "envFrom"
+	StepFieldPrivileged         = "privileged"
 	StepFieldPublishImageConfig = "publishImageConfig"
 	StepFieldRunScriptConfig    = "runScriptConfig"
 	StepFieldSourceCodeConfig   = "sourceCodeConfig"
@@ -12,6 +15,9 @@ const (
 
 type Step struct {
 	ApplyYamlConfig    *ApplyYamlConfig    `json:"applyYamlConfig,omitempty" yaml:"applyYamlConfig,omitempty"`
+	Env                map[string]string   `json:"env,omitempty" yaml:"env,omitempty"`
+	EnvFrom            []EnvFrom           `json:"envFrom,omitempty" yaml:"envFrom,omitempty"`
+	Privileged         bool                `json:"privileged,omitempty" yaml:"privileged,omitempty"`
 	PublishImageConfig *PublishImageConfig `json:"publishImageConfig,omitempty" yaml:"publishImageConfig,omitempty"`
 	RunScriptConfig    *RunScriptConfig    `json:"runScriptConfig,omitempty" yaml:"runScriptConfig,omitempty"`
 	SourceCodeConfig   *SourceCodeConfig   `json:"sourceCodeConfig,omitempty" yaml:"sourceCodeConfig,omitempty"`
