3c927f38da1e2b610564f84e915652293eb55578
gitlawr <lawrleegle@gmail.com>
2018-09-27 15:53:30 +0800

    generated

--- client/project/v3/zz_generated_bitbucket_server_apply_input.go
+++ client/project/v3/zz_generated_bitbucket_server_apply_input.go
@@ -1,12 +1,18 @@
 package client
 
 const (
-	BitbucketServerApplyInputType                 = "bitbucketServerApplyInput"
-	BitbucketServerApplyInputFieldBitbucketConfig = "bitbucketConfig"
-	BitbucketServerApplyInputFieldCode            = "code"
+	BitbucketServerApplyInputType             = "bitbucketServerApplyInput"
+	BitbucketServerApplyInputFieldHostname    = "hostname"
+	BitbucketServerApplyInputFieldPassword    = "password"
+	BitbucketServerApplyInputFieldRedirectURL = "redirectUrl"
+	BitbucketServerApplyInputFieldTLS         = "tls"
+	BitbucketServerApplyInputFieldUserName    = "username"
 )
 
 type BitbucketServerApplyInput struct {
-	BitbucketConfig *BitbucketServerPipelineConfig `json:"bitbucketConfig,omitempty" yaml:"bitbucketConfig,omitempty"`
-	Code            string                         `json:"code,omitempty" yaml:"code,omitempty"`
+	Hostname    string `json:"hostname,omitempty" yaml:"hostname,omitempty"`
+	Password    string `json:"password,omitempty" yaml:"password,omitempty"`
+	RedirectURL string `json:"redirectUrl,omitempty" yaml:"redirectUrl,omitempty"`
+	TLS         bool   `json:"tls,omitempty" yaml:"tls,omitempty"`
+	UserName    string `json:"username,omitempty" yaml:"username,omitempty"`
 }
