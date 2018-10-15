cdef1124cca9f456548827e65b95281666714073
gitlawr <lawrleegle@gmail.com>
2018-10-15 16:44:07 +0800

    update types git

--- apis/project.cattle.io/v3/pipeline_types.go
+++ apis/project.cattle.io/v3/pipeline_types.go
@@ -228,9 +228,13 @@ type ApplyYamlConfig struct {
 }
 
 type PublishCatalogConfig struct {
-	Path    string `json:"path,omitempty" yaml:"path,omitempty"`
-	Catalog string `json:"catalog,omitempty" yaml:"catalog,omitempty"`
-	Version string `json:"version,omitempty" yaml:"version,omitempty"`
+	Path      string `json:"path,omitempty" yaml:"path,omitempty"`
+	Catalog   string `json:"catalog,omitempty" yaml:"catalog,omitempty"`
+	Version   string `json:"version,omitempty" yaml:"version,omitempty"`
+	GitURL    string `json:"gitUrl,omitempty" yaml:"gitUrl,omitempty"`
+	GitBranch string `json:"gitBranch,omitempty" yaml:"gitBranch,omitempty"`
+	GitAuthor string `json:"gitAuthor,omitempty" yaml:"gitAuthor,omitempty"`
+	GitEmail  string `json:"gitEmail,omitempty" yaml:"gitEmail,omitempty"`
 }
 
 type ApplyAppConfig struct {
