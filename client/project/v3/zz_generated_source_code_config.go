77e6cf855ab7237ed58f80f292afee25010bb98d
gitlawr <lawrleegle@gmail.com>
2018-11-21 16:18:33 +0800

    generated

--- client/project/v3/zz_generated_source_code_config.go
+++ client/project/v3/zz_generated_source_code_config.go
@@ -1,8 +1,12 @@
 package client
 
 const (
-	SourceCodeConfigType = "sourceCodeConfig"
+	SourceCodeConfigType               = "sourceCodeConfig"
+	SourceCodeConfigFieldSkipClone     = "skipClone"
+	SourceCodeConfigFieldSkipTLSVerify = "skipTlsVerify"
 )
 
 type SourceCodeConfig struct {
+	SkipClone     bool `json:"skipClone,omitempty" yaml:"skipClone,omitempty"`
+	SkipTLSVerify bool `json:"skipTlsVerify,omitempty" yaml:"skipTlsVerify,omitempty"`
 }
